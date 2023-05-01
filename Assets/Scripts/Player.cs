e1df419e8dfaf2f97241955484b46a32a2793156
Peter Caceres <peter.caceresn@gmail.com>
2023-05-01 18:41:27 -0500

    add jump in wall, first version

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,11 +6,17 @@ public class Player : MonoBehaviour
 {
     
     [SerializeField]private float _movementSpeed = 10.0f;
-    [SerializeField]private float _jumpForce = 1000.0f;
+    [SerializeField]private float _jumpForce = 20.0f;
     private Animator animator;
     private SpriteRenderer sprite;
     private Rigidbody2D rigid; 
     private bool isGrounded;
+    private bool jumpInWall;
+    private float horizontalInput;
+
+    [SerializeField]private float _timer = 0.5f;
+    private float _initialTime = 0;
+    private bool _waitMove = false;
 
     void Start()
     {
@@ -23,19 +29,51 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        move();
+
+        //esperar el valor de _timer para poder moverse, 
+        //esto para que el salto del personaje sobre la pared sea mas limpio.
+        if (!_waitMove) {
+            move();
+        } else {
+            wait();
+        }
+        
         jump();
+    }
+
+    void wait() {
+        _initialTime = _initialTime + Time.deltaTime;
+
+        if (_initialTime > _timer){
+            
+            _waitMove = false;
+
+            _initialTime = 0.0f;
+
+        } else {
+            _waitMove = true;
+        }
+
     }
 
     void move() {
 
-        float horizontalInput = Input.GetAxis("Horizontal") * _movementSpeed * Time.deltaTime;
+        horizontalInput = Input.GetAxis("Horizontal") * _movementSpeed * Time.deltaTime;
+
+        Vector3 currentScale = transform.localScale;
 
         if (horizontalInput > 0 ) {
-            sprite.flipX = false;
+
+            currentScale.x = Mathf.Abs(currentScale.x);
+            transform.localScale = currentScale;
+
+            //sprite.flipX = false;
         } 
         if (horizontalInput < 0) {
-            sprite.flipX = true;
+
+            currentScale.x = Mathf.Abs(currentScale.x) * -1;
+            transform.localScale = currentScale;
+            //sprite.flipX = true;
         }
 
         animator.SetFloat("PlayerIsRunning", Mathf.Abs(horizontalInput));
@@ -50,10 +88,24 @@ public class Player : MonoBehaviour
     }
 
     void jump() {
-        if (Input.GetKeyDown("space") && isGrounded)
+
+        if (Input.GetKeyDown("space") && (isGrounded || jumpInWall))
         {
-           rigid.AddForce(Vector2.up * _jumpForce);
-           isGrounded = false;
+            rigid.AddForce(new Vector2(0, 1) * _jumpForce , ForceMode2D.Impulse);
+            Debug.Log("jumpInWall: " + jumpInWall);
+            if (jumpInWall) {
+                
+                if (horizontalInput < 0) {
+                    rigid.AddForce(new Vector2(1, 1) * _jumpForce / 2 , ForceMode2D.Impulse);
+                }
+
+                if (horizontalInput > 0) {
+                    rigid.AddForce(new Vector2(-1, 1) * _jumpForce / 2 , ForceMode2D.Impulse);
+                }
+                jumpInWall = false;
+            }
+
+            isGrounded = false;
         }
     }
 
@@ -66,6 +118,13 @@ public class Player : MonoBehaviour
          if (other.gameObject.name == "Collider" && isGrounded == false)
          {
              isGrounded = true;
+             jumpInWall = false;
+         }
+
+         if (other.gameObject.name == "Wall")
+         {
+             jumpInWall = true;
+             _waitMove = true;
          }
      }
 }
