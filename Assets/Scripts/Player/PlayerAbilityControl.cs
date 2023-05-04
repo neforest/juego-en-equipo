339b6ccb8b17298d1de3bd5dae4b78c3911b9a07
jivanrglez <jivanrglez@gmail.com>
2023-05-03 21:32:45 -0600

    Powerup merged with player. Player prefab.

--- Assets/Scripts/Player/PlayerAbilityControl.cs
+++ Assets/Scripts/Player/PlayerAbilityControl.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerAbilityControll : MonoBehaviour
+public class PlayerAbilityControl : MonoBehaviour
 {
     public bool canDoubleJump, canWallJump, canAttack;
 }
